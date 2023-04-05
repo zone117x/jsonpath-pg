c3dc7dde055bb9972d67b8bd9efb39f32c2b626e
Matthew Little <zone117x@gmail.com>
2023-04-05 16:46:28 +0200

    wip: debug build option

--- test/test-wasm.js
+++ test/test-wasm.js
@@ -1,12 +1,14 @@
 const jsonpathParseInit = require('../build/jsonpath_parse.js');
 
-async function run() {
-    const jsonpathParse = await jsonpathParseInit();
+function run() {
+    const jsonpathParse = jsonpathParseInit();
     const parseFn = jsonpathParse.cwrap('jsonpath_to_ast', 'string', ['string']);
 
-    const input = '$.asdf[1 to last].**.thing.double() ? ((@.leaf[7] - 12345) == 1 % 9)';
-    const result = parseFn(input);
-    console.log(result);
+    while (true) {
+        const input = '$.asdf[1 to last].**.thing.double() ? ((@.leaf[7] - 12345) == 1 % 9)';
+        const result = parseFn(input);
+        console.log(result);
+    }
 }
 
-run().catch(error => console.error(error));
+run();
